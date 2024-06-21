b43cfe1626b1493f80af09720bafa6a36ab0efc2
Alessio Buccino <alejoe9187@gmail.com>
2024-06-21 13:16:57 +0200

    Force 'dev' installation mode

--- src/spikeinterface/sorters/external/tests/test_singularity_containers.py
+++ src/spikeinterface/sorters/external/tests/test_singularity_containers.py
@@ -42,7 +42,9 @@ def run_kwargs():
 def test_spykingcircus(run_kwargs, create_cache_folder):
     cache_folder = create_cache_folder
     clean_singularity_cache()
-    sorting = ss.run_sorter("spykingcircus", folder=cache_folder / "spykingcircus", **run_kwargs)
+    sorting = ss.run_sorter(
+        "spykingcircus", folder=cache_folder / "spykingcircus", installation_mode="dev", **run_kwargs
+    )
     print("resulting sorting")
     print(sorting)
 
@@ -50,7 +52,9 @@ def test_spykingcircus(run_kwargs, create_cache_folder):
 def test_mountainsort4(run_kwargs, create_cache_folder):
     cache_folder = create_cache_folder
     clean_singularity_cache()
-    sorting = ss.run_sorter("mountainsort4", folder=cache_folder / "mountainsort4", **run_kwargs)
+    sorting = ss.run_sorter(
+        "mountainsort4", folder=cache_folder / "mountainsort4", installation_mode="dev", **run_kwargs
+    )
     print("resulting sorting")
     print(sorting)
 
@@ -58,7 +62,9 @@ def test_mountainsort4(run_kwargs, create_cache_folder):
 def test_mountainsort5(run_kwargs, create_cache_folder):
     cache_folder = create_cache_folder
     clean_singularity_cache()
-    sorting = ss.run_sorter("mountainsort5", folder=cache_folder / "mountainsort5", **run_kwargs)
+    sorting = ss.run_sorter(
+        "mountainsort5", folder=cache_folder / "mountainsort5", installation_mode="dev", **run_kwargs
+    )
     print("resulting sorting")
     print(sorting)
 
@@ -66,7 +72,7 @@ def test_mountainsort5(run_kwargs, create_cache_folder):
 def test_tridesclous(run_kwargs, create_cache_folder):
     cache_folder = create_cache_folder
     clean_singularity_cache()
-    sorting = ss.run_sorter("tridesclous", folder=cache_folder / "tridesclous", **run_kwargs)
+    sorting = ss.run_sorter("tridesclous", folder=cache_folder / "tridesclous", installation_mode="dev", **run_kwargs)
     print("resulting sorting")
     print(sorting)
 
@@ -74,7 +80,9 @@ def test_tridesclous(run_kwargs, create_cache_folder):
 def test_ironclust(run_kwargs, create_cache_folder):
     cache_folder = create_cache_folder
     clean_singularity_cache()
-    sorting = ss.run_sorter("ironclust", folder=cache_folder / "ironclust", fGpu=False, **run_kwargs)
+    sorting = ss.run_sorter(
+        "ironclust", folder=cache_folder / "ironclust", fGpu=False, installation_mode="dev", **run_kwargs
+    )
     print("resulting sorting")
     print(sorting)
 
@@ -82,7 +90,9 @@ def test_ironclust(run_kwargs, create_cache_folder):
 def test_waveclus(run_kwargs, create_cache_folder):
     cache_folder = create_cache_folder
     clean_singularity_cache()
-    sorting = ss.run_sorter(sorter_name="waveclus", folder=cache_folder / "waveclus", **run_kwargs)
+    sorting = ss.run_sorter(
+        sorter_name="waveclus", folder=cache_folder / "waveclus", installation_mode="dev", **run_kwargs
+    )
     print("resulting sorting")
     print(sorting)
 
@@ -90,7 +100,7 @@ def test_waveclus(run_kwargs, create_cache_folder):
 def test_hdsort(run_kwargs, create_cache_folder):
     cache_folder = create_cache_folder
     clean_singularity_cache()
-    sorting = ss.run_sorter(sorter_name="hdsort", folder=cache_folder / "hdsort", **run_kwargs)
+    sorting = ss.run_sorter(sorter_name="hdsort", folder=cache_folder / "hdsort", installation_mode="dev", **run_kwargs)
     print("resulting sorting")
     print(sorting)
 
@@ -98,7 +108,9 @@ def test_hdsort(run_kwargs, create_cache_folder):
 def test_kilosort1(run_kwargs, create_cache_folder):
     cache_folder = create_cache_folder
     clean_singularity_cache()
-    sorting = ss.run_sorter(sorter_name="kilosort", folder=cache_folder / "kilosort", useGPU=False, **run_kwargs)
+    sorting = ss.run_sorter(
+        sorter_name="kilosort", folder=cache_folder / "kilosort", useGPU=False, installation_mode="dev", **run_kwargs
+    )
     print("resulting sorting")
     print(sorting)
 
@@ -110,7 +122,9 @@ def test_combinato(run_kwargs, create_cache_folder):
     channels = rec.get_channel_ids()[0:1]
     rec_one_channel = rec.channel_slice(channels)
     run_kwargs["recording"] = rec_one_channel
-    sorting = ss.run_sorter(sorter_name="combinato", folder=cache_folder / "combinato", **run_kwargs)
+    sorting = ss.run_sorter(
+        sorter_name="combinato", folder=cache_folder / "combinato", installation_mode="dev", **run_kwargs
+    )
     print(sorting)
 
 
@@ -118,7 +132,7 @@ def test_combinato(run_kwargs, create_cache_folder):
 def test_klusta(run_kwargs, create_cache_folder):
     cache_folder = create_cache_folder
     clean_singularity_cache()
-    sorting = ss.run_sorter("klusta", folder=cache_folder / "klusta", **run_kwargs)
+    sorting = ss.run_sorter("klusta", folder=cache_folder / "klusta", installation_mode="dev", **run_kwargs)
     print(sorting)
 
 
